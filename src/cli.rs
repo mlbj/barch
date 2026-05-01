e06fc5cf81b621d010843d269994f81f6d7e851d
Mauro Brandão <mrbrjr@gmail.com>
2026-05-01 13:17:15 -0300

    make cli more informative

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,10 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(name = "bark")]
+#[command(name = "bark",
+          version,
+          about = "A minimal, headless reference manager for storing BibTex entries and associated files"
+)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
@@ -9,10 +12,40 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
+    /// Add a new reference (reads BibTeX from stdin)
     Add,
-    List { tag: Option<String> },
-    Show { input: String },
-    Export { tag: Option<String> },
-    Import { filename: String },
-    Tag { input: String, tag: String }
+
+    /// List stored references
+    List {
+        /// Filter by tag
+        #[arg(short, long)]
+        tag: Option<String> 
+    },
+
+    /// Show full BibTeX entry
+    Show {
+        /// Entry key, full id or short id
+        input: String },
+
+    /// Export references to reference.bib file
+    Export {
+        /// Filter by tag
+        #[arg(short, long)]
+        tag: Option<String>
+    },
+
+    /// Import references from a .bib file
+    Import {
+        /// Input BibTeX file
+        filename: String
+    },
+
+    /// Add a tag to a reference
+    Tag {
+        /// Entry key, full id or short id
+        input: String,
+        
+        /// Tag name
+        tag: String
+    },
 }
